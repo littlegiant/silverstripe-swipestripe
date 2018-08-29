b1687805a9c7cbaf1cc0b33ce67e6458ae591d0f
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-08-29 15:48:30 +1200

    Add option to apply order item add-ons per-unit

--- src/Order/OrderItem/OrderItemAddOn.php
+++ src/Order/OrderItem/OrderItemAddOn.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 namespace SwipeStripe\Order\OrderItem;
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\FieldType\DBBoolean;
 use SwipeStripe\AddOn;
+use SwipeStripe\Price\DBPrice;
 
 /**
  * Add on applied to on order item on a purchase. The add-on is applied once to the item's subtotal (unit price x quantity),
  * not to the unit price (i.e. add-on is not applied $quantity times).
  * @package SwipeStripe\Order\OrderItem
+ * @property bool $ApplyPerUnit
  * @property int $OrderItemID
  * @method null|OrderItem OrderItem()
  */
@@ -29,7 +32,26 @@ class OrderItemAddOn extends DataObject
     /**
      * @var array
      */
+    private static $db = [
+        'ApplyPerUnit' => DBBoolean::class,
+    ];
+
+    /**
+     * @var array
+     */
     private static $has_one = [
         'OrderItem' => OrderItem::class,
     ];
+
+    /**
+     * @inheritDoc
+     */
+    public function getAmount(): DBPrice
+    {
+        $baseAmount = $this->BaseAmount;
+
+        return $this->ApplyPerUnit
+            ? DBPrice::create_field(DBPrice::class, $baseAmount->getMoney()->multiply($this->OrderItem()->getQuantity()))
+            : $baseAmount;
+    }
 }
