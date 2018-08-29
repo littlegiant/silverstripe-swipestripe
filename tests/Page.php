66f7618e649712ceb10f9ee77aea3cece4a12639
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-08-29 12:23:55 +1200

    Add silverstripe/cms dependency and Page/Controller

---
+++ tests/Page.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Dev\TestOnly;
+
+/**
+ * Class Page
+ */
+class Page extends SiteTree implements TestOnly
+{
+}
