ce12fdfd83f5e6b36dabf3faa5e4eacbc23cd681
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-09-04 14:49:19 +1200

    Add order confirmation page

--- src/Pages/ViewOrderPage.php
+++ src/Pages/ViewOrderPage.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace SwipeStripe\Pages;
 
+use SwipeStripe\Order\Order;
+
 /**
  * Class OrderPage
  * @package SwipeStripe\Pages
@@ -10,4 +12,20 @@ namespace SwipeStripe\Pages;
 class ViewOrderPage extends \Page
 {
     use RequiredSinglePage;
+
+    /**
+     * @param Order $order
+     * @param bool $forceGuestToken Force include guest token.
+     * @return string
+     */
+    public function LinkForOrder(Order $order, bool $forceGuestToken = false): string
+    {
+        $link = $this->Link($order->ID);
+
+        if ($forceGuestToken || $order->Customer()->IsGuest()) {
+            $link .= "/{$order->GuestToken}";
+        }
+
+        return $link;
+    }
 }
