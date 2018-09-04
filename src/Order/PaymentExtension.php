7180531c0ac00e82c3029a832a6e5e770be28a25
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-09-04 14:50:09 +1200

    Fix Order relation call in PaymentExtension

--- src/Order/PaymentExtension.php
+++ src/Order/PaymentExtension.php
@@ -28,8 +28,22 @@ class PaymentExtension extends DataExtension
      */
     public function onCaptured(ServiceResponse $response): void
     {
-        if ($this->Order()->exists()) {
-            $this->Order()->paymentCaptured($this->owner, $response);
+        $order = $this->owner->Order();
+
+        if ($order->exists()) {
+            $order->paymentCaptured($this->owner, $response);
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function onCancelled(): void
+    {
+        $order = $this->owner->Order();
+        
+        if ($order->exists()) {
+            $order->paymentCancelled($this->owner);
         }
     }
 }
