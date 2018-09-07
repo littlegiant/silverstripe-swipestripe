abf4980b9a625f351d009573882de88065a9ed70
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-09-07 14:45:06 +1200

    Add tests for order empty and hash

--- tests/Order/CartTest.php
+++ tests/Order/CartTest.php
@@ -5,6 +5,7 @@ namespace SwipeStripe\Tests\Order;
 
 use SilverStripe\Dev\SapphireTest;
 use SwipeStripe\Order\Order;
+use SwipeStripe\Order\OrderItem\OrderItem;
 use SwipeStripe\Tests\DataObjects\TestProduct;
 use SwipeStripe\Tests\Price\NeedsSupportedCurrencies;
 
@@ -130,9 +131,9 @@ class CartTest extends SapphireTest
         $order->removeItem(static::$product);
         $this->assertTrue($order->Empty());
 
+        $this->assertNull($order->getOrderItem(static::$product, false));
         $orderItem = $order->getOrderItem(static::$product);
         $this->assertFalse($orderItem->exists());
-        $this->assertNull($order->getOrderItem(static::$product, false));
         $this->assertSame(0, $orderItem->getQuantity());
     }
 
@@ -165,6 +166,9 @@ class CartTest extends SapphireTest
         }
     }
 
+    /**
+     * @throws \Exception
+     */
     public function testUnlock()
     {
         $order = $this->order;
@@ -192,5 +196,72 @@ class CartTest extends SapphireTest
         $order->addItem(static::$product);
         $this->assertCount(1, $order->OrderItems());
         $this->assertSame(4, $order->getOrderItem(static::$product)->getQuantity());
+    }
+
+    /**
+     * @throws \SilverStripe\ORM\ValidationException
+     */
+    public function testHash()
+    {
+        $order = $this->order;
+        $originalHash = $order->getHash();
+
+        /** @var Order $sameOrder */
+        $sameOrder = Order::get()->byID($order->ID);
+        $differentOrder = Order::create();
+        $order->write();
+
+        // Different empty orders have different hashes
+        $this->assertSame($originalHash, $sameOrder->getHash());
+        $this->assertNotEquals($originalHash, $differentOrder->getHash());
+
+        // Test that changing from cart to order doesn't affect the hash
+        $order->IsCart = false;
+        $order->write();
+        $this->assertSame($originalHash, $order->getHash());
+
+        // Restore modifications
+        $order->IsCart = true;
+        $order->write();
+
+        // Adding item changes hash
+        $order->addItem(static::$product);
+        $this->assertNotEquals($originalHash, $order->getHash());
+
+        // Changing quantity changes hash
+        $quantity1Hash = $order->getHash();
+        $order->addItem(static::$product);
+        $this->assertNotEquals($quantity1Hash, $order->getHash());
+
+        // Revert to quantity 1 restores hash
+        $order->setItemQuantity(static::$product, 1);
+        $this->assertSame($quantity1Hash, $order->getHash());
+
+        // Revert to empty restores original empty hash
+        $order->removeItem(static::$product);
+        $this->assertSame($originalHash, $order->getHash());
+
+        // Locking doesn't change hash
+        $order->Lock();
+        $this->assertSame($originalHash, $order->getHash());
+    }
+
+    public function testEmpty()
+    {
+       $order = $this->order;
+
+       $this->assertTrue($order->Empty());
+
+       $order->setItemQuantity(static::$product, 0);
+       $this->assertCount(1, $order->OrderItems());
+       $this->assertInstanceOf(OrderItem::class, $order->getOrderItem(static::$product, false));
+       $this->assertSame(0, $order->getOrderItem(static::$product)->getQuantity());
+       $this->assertTrue($order->Empty());
+
+       $order->addItem(static::$product);
+       $this->assertFalse($order->Empty());
+
+       $order->removeItem(static::$product);
+       $this->assertTrue($order->Empty());
     }
 }
