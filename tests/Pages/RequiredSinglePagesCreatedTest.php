4376f9dc4ca58c57c65b77d89f30b31ea3c1e33e
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-09-10 14:13:37 +1200

    Add test for eCommerce default page records

---
+++ tests/Pages/RequiredSinglePagesCreatedTest.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace SwipeStripe\Tests\Pages;
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Dev\SapphireTest;
+use SwipeStripe\Pages\CheckoutPage;
+use SwipeStripe\Pages\OrderConfirmationPage;
+use SwipeStripe\Pages\ViewCartPage;
+use SwipeStripe\Pages\ViewOrderPage;
+
+/**
+ * Class RequiredSinglePagesCreatedTest
+ * @package SwipeStripe\Tests\Pages
+ */
+class RequiredSinglePagesCreatedTest extends SapphireTest
+{
+    /**
+     * @var bool
+     */
+    protected $usesDatabase = true;
+
+    /**
+     *
+     */
+    public function testViewOrderPage()
+    {
+        static::assertDefaultRecordForPage($this, ViewOrderPage::class);
+    }
+
+    /**
+     * @param \PHPUnit_Framework_TestCase $test
+     * @param string $pageClass
+     */
+    public static function assertDefaultRecordForPage(\PHPUnit_Framework_TestCase $test, string $pageClass)
+    {
+        $test->assertNull(SiteTree::get_one($pageClass, '', false));
+        SiteTree::singleton($pageClass)->requireDefaultRecords();
+        $test->assertInstanceOf($pageClass, SiteTree::get_one($pageClass, '', false));
+    }
+
+    /**
+     *
+     */
+    public function testViewCartPage()
+    {
+        static::assertDefaultRecordForPage($this, ViewCartPage::class);
+    }
+
+    /**
+     *
+     */
+    public function testOrderConfirmationPage()
+    {
+        static::assertDefaultRecordForPage($this, OrderConfirmationPage::class);
+    }
+
+    /**
+     *
+     */
+    public function testCheckoutPage()
+    {
+        static::assertDefaultRecordForPage($this, CheckoutPage::class);
+    }
+}
