b9e193071c56c6b0efdeed0770a34ab567f3ec6b
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-09-14 13:00:57 +1200

    Add DBAddress composite field

---
+++ src/ORM/FieldType/DBAddress.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace SwipeStripe\ORM\FieldType;
+
+use Dynamic\CountryDropdownField\Fields\CountryDropdownField;
+use SilverStripe\Forms\FieldGroup;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\FieldType\DBComposite;
+use SilverStripe\ORM\FieldType\DBVarchar;
+
+/**
+ * Class DBAddress
+ * @package SwipeStripe\ORM\FieldType
+ * @property string $Unit
+ * @property string $Street
+ * @property string $Suburb
+ * @property string $City
+ * @property string $Region
+ * @property string $Postcode
+ * @property string $Country
+ */
+class DBAddress extends DBComposite
+{
+    /**
+     * @var array
+     */
+    private static $composite_db = [
+        'Unit'     => DBVarchar::class,
+        'Street'   => DBVarchar::class,
+        'Suburb'   => DBVarchar::class,
+        'City'     => DBVarchar::class,
+        'Region'   => DBVarchar::class,
+        'Postcode' => DBVarchar::class,
+        'Country'  => DBVarchar::class,
+    ];
+
+    /**
+     * @return string
+     */
+    public function Nice(): string
+    {
+        return <<<EOT
+{$this->Unit} {$this->Street},
+{$this->Suburb},
+{$this->City},
+{$this->Region},
+{$this->Country} {$this->Postcode}
+EOT;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function scaffoldFormField($title = null, $params = null)
+    {
+        return FieldGroup::create([
+            TextField::create("{$this->getName()}Unit", 'Unit'),
+            TextField::create("{$this->getName()}Street", 'Street'),
+            TextField::create("{$this->getName()}Suburb", 'Suburb'),
+            TextField::create("{$this->getName()}City", 'City'),
+            TextField::create("{$this->getName()}Region", 'Region'),
+            TextField::create("{$this->getName()}Postcode", 'Post Code / Zip'),
+            CountryDropdownField::create("{$this->getName()}Country", 'Country'),
+        ]);
+    }
+}
