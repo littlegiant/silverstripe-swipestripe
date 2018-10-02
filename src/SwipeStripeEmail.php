3fed071a6a195ab1ef1411db6af49ac153c681c2
Harsh Chokshi <hchokshi@users.noreply.github.com>
2018-10-02 16:05:56 +1300

    Use $Layout in email templates to allow shared base structure

--- src/SwipeStripeEmail.php
+++ src/SwipeStripeEmail.php
@@ -18,6 +18,12 @@ class SwipeStripeEmail extends Email
     private static $default_sender;
 
     /**
+     * @see Email::$HTMLTemplate (is private, so can't )
+     * @var string|null
+     */
+    protected $HTMLTemplateOverride;
+
+    /**
      * @inheritDoc
      * TODO - add CMS editable (siteconfig?) default sender of store emails
      */
@@ -33,5 +39,32 @@ class SwipeStripeEmail extends Email
         $from = $from ?? static::config()->get('default_sender');
 
         parent::__construct($from, $to, $subject, $body, $cc, $bcc, $returnPath);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHTMLTemplate()
+    {
+        if ($this->HTMLTemplateOverride) {
+            return $this->HTMLTemplateOverride;
+        }
+
+        return $this->getViewerTemplates();
+    }
+
+    /**
+     * @inheritDoc
+     * @param string|array $template
+     */
+    public function setHTMLTemplate($template)
+    {
+        if (is_string($template) && substr($template, -3) == '.ss') {
+            $template = substr($template, 0, -3);
+        }
+
+        $this->HTMLTemplateOverride = $template;
+
+        return $this;
     }
 }
--- templates/SwipeStripe/SwipeStripeEmail.ss
+++ templates/SwipeStripe/SwipeStripeEmail.ss
@@ -6,14 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-    <% with $Order %>
-        <p>Hey {$CustomerName},</p>
-
-        <p>We have received your order and payment for order #{$ID}.</p>
-
-        <p>View your order here - <a href="{$AbsoluteLink}">{$AbsoluteLink}</a>.</p>
-
-        <!-- TODO - items, payment details, etc. -->
-    <% end_with %>
+    {$Layout}
 </body>
 </html>
